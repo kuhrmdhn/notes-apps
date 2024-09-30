112a5cc735f6ad9117eeffc519c95606c6b2ebb9
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-09-30 08:08:00 +0700

    add empty note handling ui

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,3 +1,3 @@
 export default function Logo() {
-  return <h1 className="font-bold text-2xl text-slate-800"><span className='text-red-main'>i</span>Notes</h1>
+  return <h1 className="font-bold text-2xl text-slate-800"><span className='text-red-main'>i</span>Note</h1>
 }
